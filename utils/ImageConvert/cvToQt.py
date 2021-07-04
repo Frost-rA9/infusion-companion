08b024b71d2546395d29258d6e98dd320de343a1
Frost-rA9 <Frost.rA9@gmail.com>
2021-07-04 16:36:23 +0800

    opencv 与 Qt 图像转换

---
+++ utils/ImageConvert/cvToQt.py
@@ -0,0 +1,11 @@
+import cv2
+from PyQt5 import QtGui
+
+
+class cvToQt:
+    @staticmethod
+    def cvImgToQtImg(cvImg):
+        QtImgBuf = cv2.cvtColor(cvImg, cv2.COLOR_BGR2BGRA)
+        QtImg = QtGui.QImage(QtImgBuf.data, QtImgBuf.shape[1], QtImgBuf.shape[0], QtGui.QImage.Format_RGB32)
+
+        return QtImg
