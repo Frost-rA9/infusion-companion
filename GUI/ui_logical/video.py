4625008d53ae02331842c4d3dfc58c141acfc7bc
Frost-rA9 <Frost.rA9@gmail.com>
2021-07-04 17:53:23 +0800

    图像转换与图像显示修正

--- GUI/ui_logical/video.py
+++ GUI/ui_logical/video.py
@@ -4,6 +4,7 @@ from GUI.GUIHelper.QtHelper import QtHelper
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QObject, Signal
 from PySide2.QtGui import QImage
+from PySide2.QtGui import QPixmap
 import numpy as np
 
 
@@ -24,14 +25,12 @@ if __name__ == '__main__':
     img = "../../Resource/CAER-S/Test/Anger/0001.png"
     import cv2 as cv
     from GUI.GUIHelper.QtImgConvert import QtImgConvert
-
     i = cv.imread(img)
-    # cv.imshow("i", i)
-    # cv.waitKey(0)
     convert = QtImgConvert.ndarray_to_QImage(i)
-
     app = QApplication()
     v = video()
-    v.ui_video.video.setPicture(convert)
+    pix = QPixmap("../../Resource/CAER-S/Test/Anger/0001.png")
+    v.ui_video.video.setScaledContents(True)
+    v.ui_video.video.setPixmap(QPixmap.fromImage(convert))
     v.ui_video.show()
     app.exec_()
