d605e48ca321976138855c602e56a91a6bf17c35
tacom <1017208039@qq.com>
2021-07-04 15:55:54 +0800

    代码存档

---
+++ GUI/GUIHelper/QtImgConvert.py
@@ -0,0 +1,27 @@
+# encoding=GBK
+"""QtImgConvert
+
+    - 主要作用是完成opencv的numpy.ndarray到QImage
+    - 还有反方向的相互转换
+
+    - 提供静态函数
+"""
+
+import numpy as np
+from PySide2.QtGui import QImage
+import cv2 as cv
+
+
+class QtImgConvert:
+    @staticmethod
+    def ndarray_to_QImage(array_img: np.ndarray):
+        height, width, depth = array_img.shape
+        array_img = cv.cvtColor(array_img, cv.COLOR_BGR2RGB)
+        q_img = QImage(array_img.data,
+                       width, height, depth,
+                       QImage.Format_RGB888)
+        return q_img
+
+    @staticmethod
+    def QImage_to_ndarray():
+        pass
--- GUI/ui_logical/body.py
+++ GUI/ui_logical/body.py
@@ -4,7 +4,6 @@ from PySide2.QtWidgets import QApplication, QVBoxLayout
 
 class body:
     def __init__(self,
-                 parent=None,
                  ui_path: str = None):
         if ui_path:
             self.ui_body = QtHelper.read_ui(ui_path)
