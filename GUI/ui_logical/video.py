619afc37363469106adc1376a4bdd948a86b8a2d
Frost-rA9 <Frost.rA9@gmail.com>
2021-07-04 18:55:59 +0800

    pyqt视频流播放

--- GUI/ui_logical/video.py
+++ GUI/ui_logical/video.py
@@ -1,10 +1,10 @@
 # encoding=GBK
-
+from GUI.GUIHelper.QtImgConvert import QtImgConvert
 from GUI.GUIHelper.QtHelper import QtHelper
 from PySide2.QtWidgets import QApplication
 from PySide2.QtCore import QObject, Signal
-from PySide2.QtGui import QImage
 from PySide2.QtGui import QPixmap
+import cv2 as cv
 import numpy as np
 
 
@@ -23,14 +23,25 @@ class video:
 
 if __name__ == '__main__':
     img = "../../Resource/CAER-S/Test/Anger/0001.png"
-    import cv2 as cv
-    from GUI.GUIHelper.QtImgConvert import QtImgConvert
     i = cv.imread(img)
     convert = QtImgConvert.ndarray_to_QImage(i)
+    vid = "../../Resource/CAER/TEST/Anger/0001.avi"
+    cap = cv.VideoCapture(vid)
     app = QApplication()
     v = video()
-    pix = QPixmap("../../Resource/CAER-S/Test/Anger/0004.png")
-    v.ui_video.video.setScaledContents(True)
-    v.ui_video.video.setPixmap(QPixmap.fromImage(convert))
-    v.ui_video.show()
+    ret, frame = cap.read()
+    while ret:
+        conv = QtImgConvert.ndarray_to_QImage(frame)
+        v.ui_video.video.setScaledContents(True)
+        v.ui_video.video.setPixmap(QPixmap.fromImage(conv))
+        ret, frame = cap.read()
+        v.ui_video.show()
+        cv.waitKey(30)
+    #     cv.imshow("test",frame)
+    #     cv.waitKey(30)
+    # cap.release()
+    # cv.destroyAllWindows()
+    # v.ui_video.video.setScaledContents(True)
+    # v.ui_video.video.setPixmap(QPixmap.fromImage(convert))
+    # v.ui_video.show()
     app.exec_()
