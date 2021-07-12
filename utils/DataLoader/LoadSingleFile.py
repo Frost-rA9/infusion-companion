1465ae2863e2750965bfa2070eb91ef3ff04287d
liu-xiang-3319 <331988510@qq.com>
2021-07-12 19:15:27 +0800

    aaa

--- utils/DataLoader/LoadSingleFile.py
+++ utils/DataLoader/LoadSingleFile.py
@@ -26,37 +26,30 @@ from PIL import Image
 import numpy as np
 import os
 import torch
-
+import cv2
 
 class LoadSingleFile(Dataset):
     def __init__(self,
                  train_path: str,
                  test_path: str,
                  is_train: bool,
-                 label_dict: dict = None,
-                 trans = None):
-        if label_dict:
-            self.label_dict = label_dict
-        else:
-            self.label_dict = {
-                "Anger": 0,
-                "Disgust": 1,
-                "Fear": 2,
-                "Happy": 3,
-                "Neutral": 4,
-                "Sad": 5,
-                "Surprise": 6,
-                "Other": 7,
-            }
+                 trans = None,
+                 resize=False):
 
+        # 0 anger 生气
+        # 1 disgust 厌恶
+        # 2 fear 恐惧
+        # 3 happy 开心
+        # 4 sad 伤心
+        # 5 surprised 惊讶
+        # 6 normal 中性
         self.train_path = train_path
         self.test_path = test_path
         self.is_train = is_train
-
         self.img_list = []
         self.img_data_load()
-
         self.trans = trans
+        self.resize = resize
 
     def img_data_load(self):
         """遍历所有的目录, 并添加类别"""
@@ -69,24 +62,34 @@ class LoadSingleFile(Dataset):
             data_path = self.test_path
 
         for file_dir in d:
-            class_number = self.label_dict.get(file_dir, 7)
+            class_number = file_dir
             absolute_path = data_path + '/' + file_dir
             for file_name in os.listdir(absolute_path):
                 file_path = absolute_path + "/" + file_name
-                self.img_list.append((file_path, class_number))
-
+                self.img_list.append((file_path, int(class_number)))
     def get_num_classes(self):
-        return len(self.label_dict) - 1
+        return 7
 
     def __getitem__(self, index):
         img_path, label = self.img_list[index]
         img = Image.open(img_path)
+        if self.resize:
+            img=img.resize((400, 400),Image.ANTIALIAS)
         if self.trans:
+                # img=np.array(img)
+                # img_expend=np.zeros((299, 299), dtype=np.uint8)
+                # img_expend[:48,:48]=img
+                # img=img_expend
+                # img = Image.fromarray(img)
             img = self.trans(img)
+
         return img, torch.tensor(label)
 
     def __len__(self):
         return len(self.img_list)
+
+
+
 
 
 if __name__ == '__main__':
@@ -95,10 +98,10 @@ if __name__ == '__main__':
     ])
     l = LoadSingleFile(train_path="../../Resource/CAER-S/train",
                        test_path="../../Resource/CAER-S/test",
-                       is_train=True,
+                       is_train=False,
                        trans=trans)
 
-    img, label = l.__getitem__(0)
+    img, label = l.__getitem__(i)
 
     print(img, label)
     print(type(img), type(label))
